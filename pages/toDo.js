dda47a248133afbdc8c854e3cae55e7c0cd1a53e
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-04-26 12:15:19 +0200

    Add new food&form-and display it

--- pages/toDo.js
+++ pages/toDo.js
@@ -1,46 +1,3 @@
-// import React, { useState, useEffect } from 'react'
-
-// export default function toDo() {
-//   const [todos, setTodos] = useState([])
-//   const [tempText, setTempText] = useState()
-
-//   // att do do function
-//   const AddTodo = (text, string) => {
-//     //keep todos and append text as new todo
-//     setTodos([todos, text])
-//   }
-
-//   useEffect(() => {
-//     console.log(todos)
-//   }, [todos])
-//   //e.target.value kan vi änvanda för hämta data från form in på ett dynamisk sett
-//   return (
-//     <div style={{ display: 'flex' }}>
-//       <input
-//         type="text"
-//         value={tempText}
-//         placeholder="Add to do"
-//         onChange={(e) => {
-//           setTempText(e.target.value)
-//         }}
-//       />
-//       <button
-//         onClick={() => {
-//           AddTodo(tempText)
-//           setTempText('')
-//         }}
-//       >
-//         Add
-//       </button>
-//       <li>
-//         {todos.map((elem, i) => {
-//           return <li>{elem}</li>
-//         })}
-//       </li>
-//     </div>
-//   )
-// }
-
 import React, { useEffect, useState } from 'react'
 
 export default function Home() {
@@ -77,15 +34,10 @@ export default function Home() {
       </button>
 
       <li>
-          {
-              todo&&todo.map((text,i)=>{
-                  return(
-                      <h1>{text}</h1>
-                  )
-
-              })
-
-          }
+        {todo &&
+          todo.map((text, i) => {
+            return <h1>{text}</h1>
+          })}
       </li>
     </div>
   )
