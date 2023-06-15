fe47b99ca3622b031b464783797bfa8d68da2c42
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-06-15 19:21:28 +0200

    getstaticProps

--- pages/dishes/index.js
+++ pages/dishes/index.js
@@ -1,25 +1,35 @@
+// import React from 'react'
+//  import Layout from '../../components/Layout'
+
+
+// export default function Dishes() {
+//   return (
+//     <h1>Dishes</h1>
+//   )
+// }
+
+// API_KEY = "94503ca481d649bea8a5f8917aeb0c44"
+// export async function getStaticProps() {
+//      const URL = `https://api.spoonacular.com/food/api-key=${API_KEY}`
+//      const response = await fetch(URL)
+
+//      const data = await response.json()
+//      console.log(data.results)
+
+//     return {
+//         props:{
+//             results:data.results
+//         }
+//     }
+
+// }
+
 import React from 'react'
- import Layout from '../../components/Layout'
 
-
-export default function Dishes() {
+export default function Home() {
   return (
-    <h1>Dishes</h1>
+    <div>
+      
+    </div>
   )
-}
-
-API_KEY = "94503ca481d649bea8a5f8917aeb0c44"
-export async function getStaticProps() {
-     const URL = `https://api.spoonacular.com/food/api-key=${API_KEY}`
-     const response = await fetch(URL)
-
-     const data = await response.json()
-     console.log(data.results)
-
-    return {
-        props:{
-            results:data.results
-        }
-    }
-
 }
